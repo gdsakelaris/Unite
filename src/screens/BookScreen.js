84dbc9a966d0530f12300482d430fd9e2878478e
Muyan(Sam) Xie <72187977+MuyanXie@users.noreply.github.com>
2023-04-22 16:36:37 -0500

    Revert "Revert "update to the figma demo version ""

---
+++ src/screens/BookScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const BookScreen = () => {
+  return (
+    <View style={{ flex: 1 }}>
+      <Text>Book Screen</Text>
+    </View>
+  );
+};
+
+export default BookScreen;
