f54a3ff4c939027c753d51b55618fd54de0c0cdd
Jesse Ge <71725877+jesse1337@users.noreply.github.com>
2023-05-09 19:54:35 -0700

    Education Screen

--- App.js
+++ App.js
@@ -1,8 +1,19 @@
-import React from "react";
-import EducationScreen from "./src/screens/EducationScreen";
+import React, { useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import LoginScreen from "./src/screens/LoginScreen";
+import BottomNavigation from "./src/components/BottomNavigation";
+import Profile from "./src/screens/Profile";
 
 function App() {
-  return <EducationScreen />;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
+  // return isLoggedIn ? <BottomNavigation /> : <LoginScreen onLogin={handleLogin} />;
+  return <Profile></Profile>;
 }
 
 export default App;
