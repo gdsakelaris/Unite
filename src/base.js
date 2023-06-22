230e6b3895d8c3f7cce84bc218197bf50a33b097
PhongDiep <phongdiep0712@gmail.com>
2023-06-22 16:51:18 -0700

    Created a base.js file to store all the neccessary properties that will be used repeatedly throughout the project

---
+++ src/base.js
@@ -0,0 +1,15 @@
+//This file will contain all color references as well as several libraries that will be used repeatedly throughout the app.
+import { PixelRatio } from "react-native"
+
+//this function will return the font scale of the user's device
+//the returned property of this function will make the text resizes automatically based on the device's fontSize preference
+export const fontScale = PixelRatio.getFontScale()
+
+//an object of color references
+export const colors = {
+  lightorange: '#F78154',
+  white: '#FFFFFF',
+  darkorange: '#F24E1E',
+  lightgray: '#F4F4F4',
+  lightdark: '#4E4E4E'
+}
