2fd65f8a3c15abcb4d672db7739c7a04508eb528
PhongDiep <phongdiep0712@gmail.com>
2023-06-27 20:37:51 -0700

    Updated App.js file  after refactoring

--- App.js
+++ App.js
@@ -1,14 +1,8 @@
 // App.js:
 import React, { useState } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import LoginScreen from './src/screens/LoginSignupScreen/LoginScreen';
-import SignUpScreen from './src/screens/LoginSignupScreen/SignUpScreen';
+import LoginSignupScreen from './src/screens/LoginSignupScreen/LoginSignupScreen';
 import BottomNavigation from './src/navigations/BottomNavigation'
 import { PaperProvider } from 'react-native-paper';
-
-const Stack = createStackNavigator();
-
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
@@ -22,14 +16,7 @@ function App() {
   } else {
     return (
     <PaperProvider>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Login">
-          <Stack.Screen name="Login" options={{ headerShown: false }}>
-            {(props) => <LoginScreen {...props} onLogin={handleLogin} />}
-          </Stack.Screen>
-          <Stack.Screen name="SignUp" component={SignUpScreen} options={{ headerShown: false }} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <LoginSignupScreen/>
     </PaperProvider>
     );
   }
