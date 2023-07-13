7278635993ec4e48dbf5598a1216a9c41292d05f
wantingxu <xu.wan@northeastern.edu>
2023-07-13 13:35:08 -0700

    modify styles of buttons in the flow of publishing resource

--- src/screens/volunteerScreens/PublishedServiceScreen/icons.js
+++ src/screens/volunteerScreens/PublishedServiceScreen/icons.js
@@ -1,4 +1,4 @@
 //icons for Published Service screen
 import renderExpoIcon from "../../../utils/renderExpoIcon";
 import { fontScale, colors } from "../../../base";
-export const addIcon =  renderExpoIcon('AntDesign', 'plus', {size: 34 / fontScale, color: colors.white})
+export const addIcon =  renderExpoIcon('AntDesign', 'plus', {size: 25 / fontScale, color: colors.white, style: { fontWeight: 'bold' }});
--- src/screens/volunteerScreens/ThirdStepScreen/css.js
+++ src/screens/volunteerScreens/ThirdStepScreen/css.js
@@ -15,18 +15,22 @@ export const title = StyleSheet.create({
 
 export const submitBtn = StyleSheet.create({
   publishBtn: {
-    width:'80%',
+    width:'60%',
     height: '6%',
     alignSelf:'center',
     justifyContent:'center',
     bottom:'5%',
     borderWidth: 0,
     alignItems: 'center',
+    shadowColor: "rgba(0, 0, 0, 0.25)",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 1,
+    shadowRadius: 4,
 
   },
   publishBtnText: {
     color: colors.white, 
-    fontSize: 25 / fontScale,
+    fontSize: 22 / fontScale,
     fontWeight:'700',
     paddingHorizontal:'4%',
     paddingVertical:'1%',
