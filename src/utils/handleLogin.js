84030d974e7f467e320a6797ae43d35008379fca
wantingxu <xu.wan@northeastern.edu>
2023-07-18 15:03:50 -0700

    reverse ip

--- src/utils/handleLogin.js
+++ src/utils/handleLogin.js
@@ -4,8 +4,7 @@ import { Alert } from "react-native";
 export default handleLogin = (onLogin, email, password) => {
   // Insert your IP below
   axios
-    // .post("http://<IP Address>:5000/login", {
-      .post("http://10.0.0.249:3000/login", {
+    .post("http://<IP Address>:5000/login", {
       email,
       password,
     })
